ab356811f3a7b13c6c11540ef7ac3b1de5661b37
TalaatMagdy <TalaatMagdy@users.noreply.github.com>
2017-07-11 20:15:21 +0200

    Update break_time.py

--- break_time.py
+++ break_time.py
@@ -6,5 +6,5 @@ break_count = 0
 print("This program started om " +time.ctime())
 while (break_count < total_breaks ):
     time.sleep(5) #delay 5 second
-    webbrowser.open_new_tab("https://www.facebook.com")
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=OG2eGVt6v2o")
     break_count = break_count + 1
