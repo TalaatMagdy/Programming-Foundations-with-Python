e39a8a2e27027dddfad335c2b4e3114ee6ba9f73
TalaatMagdy <talaatmagdy75@gmail.com>
2017-07-11 20:00:40 +0200

    break_time

---
+++ break_time.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+print("This program started om " +time.ctime())
+while (break_count < total_breaks ):
+    time.sleep(5) #delay 5 second
+    webbrowser.open_new_tab("https://www.facebook.com")
+    break_count = break_count + 1
